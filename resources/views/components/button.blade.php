9e0dd785abbe9b24679aed3ff36c69bfe9b92d4d
martingalian <martingalian@users.noreply.github.com>
2026-02-02 18:12:46 +0000

    Add spinner component and icon slot to button
    - Add new spinner component with size/color variants
    - Update button component to support icon slot
    - Add gap classes to button sizes for icon spacing

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -17,9 +17,9 @@
     ];
 
     $sizes = [
-        'sm' => 'px-3 py-1.5 text-xs',
-        'md' => 'px-4 py-2 text-sm',
-        'lg' => 'px-6 py-3 text-base',
+        'sm' => 'px-3 py-1.5 text-xs gap-1.5',
+        'md' => 'px-4 py-2 text-sm gap-2',
+        'lg' => 'px-6 py-3 text-base gap-2.5',
     ];
 
     $variantClass = $variants[$variant] ?? $variants['primary'];
@@ -37,10 +37,9 @@
 @if($href && !$disabled)
     <a href="{{ $href }}" {{ $attributes->merge(['class' => $classes]) }}>
         @if($loading)
-            <svg class="animate-spin -ml-1 mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
+            <x-ui::spinner size="sm" />
+        @elseif(isset($icon))
+            {{ $icon }}
         @endif
         {{ $slot }}
     </a>
@@ -51,10 +50,9 @@
         {{ $attributes->merge(['class' => $classes]) }}
     >
         @if($loading)
-            <svg class="animate-spin -ml-1 mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
+            <x-ui::spinner size="sm" />
+        @elseif(isset($icon))
+            {{ $icon }}
         @endif
         {{ $slot }}
     </button>
